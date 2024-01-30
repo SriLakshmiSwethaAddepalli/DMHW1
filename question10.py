bce2b92f659f0ad2562eff758ede7c5df9aa5471
SriLakshmiSwethaAddepalli <158012350+SriLakshmiSwethaAddepalli@users.noreply.github.com>
2024-01-30 05:32:09 +0530

    Update question10.py

--- question10.py
+++ question10.py
@@ -1,18 +1,22 @@
 def question10_1():
     answer = False
-    return answer
+    justify = 'Because count data can be affected by noise as it may cause missing errors, interpretation errors and also it effects the accuracy of the analysis.'
+    return answer,justify
 
 def question10_2():
     answer = False
-    return answer
+    justify = 'Because correlation requires finite variance, considers linear relationships, and needs more than 1 data point, all of which might lead to values outside the -1 to 1 range. '
+    return answer,justify
 
 def question10_3():
     answer = False
-    return answer
+    justify = 'Aggregating reduces the size while capturing trends, while sampling may lose valuable information. Choose aggregation for size reduction is a better option.'
+    return answer,justify
 
 def question10_4():
     answer = True
-    return answer
+    justify = 'Some outliers can be caused by random noise, while others might be genuine observations far from the typical range.'
+    return answer,justify
 
 def question10_5():
     answer = False
